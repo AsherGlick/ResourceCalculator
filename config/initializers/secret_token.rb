39450df2034b9c2b050cd81a177d5fe6f3672ff1
Asher Glick <asher.glick@gmail.com>
2016-08-26 20:06:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResourceCalculator::Application.config.secret_key_base = '58ed58726e8d43fc92fcb5e1f54ff98214be78cf3e57107dada971429527660425833de6fb2b1c681db64b7b5a4660224a3669319d91e0922650ec3e79265de4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ResourceCalculator::Application.config.session_store :cookie_store, key: '_resource_calculator_session'
