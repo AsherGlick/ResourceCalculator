f19e21e8b314bcb5716afd5883481f0a9359acf4
Asher Glick <asher.glick@gmail.com>
2017-06-17 19:16:21 -0400

    file loading and compression optimizatoins

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module ResourceCalculator
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.eager_load_paths += ["#{Rails.root}/lib"]
+    config.middleware.use Rack::Deflater
   end
 end
