7ee5607e16e41a66e3bc7fbf1f2d04a42517efb7
Asher Glick <asher.glick@gmail.com>
2022-06-11 21:45:39 -0500

    temporary debugging for docker build.sh

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+# if [ "$UID" -eq 0 ]
+#   then echo "This is running as root but probably should not be"
+#   exit
+# fi
+
+
+echo $UID : $GID
+
 # Install or update all dependencies.
 npm install
 if [ ! -d "venv_docker" ]; then python3 -m venv venv_docker; fi
@@ -7,4 +15,5 @@ source venv_docker/bin/activate
 pip install --quiet --no-cache-dir --requirement requirements.txt
 
 # Run the build script.
+echo "ARGS:" "$@"
 python -u build.py "$@"
