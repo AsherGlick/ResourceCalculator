e504d4f9440ded87ce3d40b9c921cf56fa784105
Asher Glick <asher.glick@gmail.com>
2024-06-25 00:06:03 -0500

    redesigning the code that makes colored text in terminals

--- pylib/terminal_color.py
+++ pylib/terminal_color.py
@@ -1,31 +1,101 @@
-FG_BLACK       = "\033[0;30m"
-FG_BLACK_BOLD  = "\033[1;30m"
-FG_RED         = "\033[0;31m"
-FG_RED_BOLD    = "\033[1;31m"
-FG_GREEN       = "\033[0;32m"
-FG_GREEN_BOLD  = "\033[1;32m"
-FG_YELLOW      = "\033[0;33m"
-FG_YELLOW_BOLD = "\033[1;33m"
-FG_BLUE        = "\033[0;34m"
-FG_BLUE_BOLD   = "\033[1;34m"
-FG_PURPLE      = "\033[0;35m"
-FG_PURPLE_BOLD = "\033[1;35m"
-FG_CYAN        = "\033[0;36m"
-FG_CYAN_BOLD   = "\033[0;36m"
-FG_WHITE       = "\033[0;37m"
-FG_WHITE_BOLD  = "\033[1;37m"
+import enum
+from typing import Optional
 
-BLACK  = 0
-RED    = 1
-GREEN  = 2
-YELLOW = 3
-BLUE   = 4
-PURPLE = 5
-CYAN   = 6
-WHITE  = 7
 
+################################################################################
+# TerminalColor
+#
+# An enum class that represents all of the colors that can be displayed in an
+# ANSII terminal, mapped to the stringy integer that represents them.
+################################################################################
+class TerminalColor(enum.Enum):
+    black = "0"
+    red = "1"
+    green = "2"
+    yellow = "3"
+    blue = "4"
+    purple = "5"
+    cyan = "6"
+    white = "7"
+
+    default = "9"
+
+
+# Helper variable to reset any color
 RESET = "\033[0m"
 
 
+################################################################################
+# get_color_escape_codes
+#
+# Creates an ANSII color escape code given different parameters about the color
+# to be used.
+################################################################################
+def get_color_escape_code(
+    bold: bool,
+    foreground_color: Optional[TerminalColor],
+    background_color: Optional[TerminalColor]
+) -> str:
+    return "".join([
+        "\033[",
+        ("1" if bold else "0"),
+        (";3" + foreground_color.value if foreground_color is not None else ""),
+        (";4" + background_color.value if background_color is not None else ""),
+        "m",
+    ])
+
+
+################################################################################
+# get_colored_text
+#
+# A function that wraps some input text in color escape codes.
+################################################################################
+def get_colored_text(
+    text: str,
+    bold: bool,
+    foreground_color: Optional[TerminalColor],
+    background_color: Optional[TerminalColor]
+) -> str:
+    return get_color_escape_code(bold, foreground_color, background_color) + text + RESET
+
+
+################################################################################
+# print_color_cube
+#
+# A helper text function that prints out all the color combinations to the
+# terminal to test the terminal's color display.
+################################################################################
+def print_color_cube() -> None:
+    colors = {
+        TerminalColor.black: "K",
+        TerminalColor.red: "R",
+        TerminalColor.green: "G",
+        TerminalColor.yellow: "Y",
+        TerminalColor.blue: "B",
+        TerminalColor.purple: "P",
+        TerminalColor.cyan: "C",
+        TerminalColor.white: "W",
+        TerminalColor.default: "D",
+        None: "N",
+    }
+
+    for bg, bg_shortcut in colors.items():
+        for bold in [False, True]:
+            for fg, fg_shortcut in colors.items():
+                color_code = ("1" if bold else "0") + fg_shortcut + bg_shortcut
+                print(get_colored_text(color_code, bold, fg, bg), end="")
+        print()
+
+
+################################################################################
+# fg_gray
+#
+# A helper function for getting a grey colored text by making a bold black text
+################################################################################
 def fg_gray(string: str) -> str:
-    return FG_BLACK_BOLD + string + RESET
+    return get_colored_text(
+        text=string,
+        bold=True,
+        foreground_color=TerminalColor.black,
+        background_color=None,
+    )
