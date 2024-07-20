c17ac0e343ec9b39345c58a47dd1389c2c87b2e7
Asher Glick <asher.glick@gmail.com>
2024-07-20 08:58:02 -0500

    adding type hints to the resource list formatter script

--- scripts/resource_list_formatter.py
+++ scripts/resource_list_formatter.py
@@ -2,18 +2,17 @@
 # Simple script that loads and saves the yaml resource file to create
 # consistent formatting across them
 ################################################################################
+import argparse
+import difflib
+import os
 import sys
+import yaml
 sys.path.append("../")
 from pylib.resource_list import ResourceList
-import yaml
 from pylib.yaml_token_load import ordered_load
-import yaml
-import difflib
-import argparse
-import os
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(
         description='Format resources.yaml files'
     )
@@ -32,13 +31,12 @@ def main():
         format_single_file(args.path, args.inline)
 
 
-def format_single_file(input_filepath: str, same_file: bool=False):
+def format_single_file(input_filepath: str, same_file: bool = False) -> None:
 
     if same_file:
         output_filepath = input_filepath
     else:
         output_filepath = input_filepath + ".formatted"
-
 
     errors = []
     with open(input_filepath, 'r', encoding="utf_8") as f:
@@ -57,14 +55,14 @@ def format_single_file(input_filepath: str, same_file: bool=False):
     print(diff_yaml(input_filepath, output_filepath))
 
 
-def diff_yaml(file_a_path: str, file_b_path: str):
+def diff_yaml(file_a_path: str, file_b_path: str) -> str:
     with open(file_a_path, 'r', encoding="utf_8") as f:
         data_a = yaml.dump(yaml.safe_load(f.read()), default_flow_style=False).splitlines()
     with open(file_b_path, 'r', encoding="utf_8") as f:
         data_b = yaml.dump(yaml.safe_load(f.read()), default_flow_style=False).splitlines()
 
     diff = difflib.unified_diff(data_a, data_b, fromfile=file_a_path, tofile=file_b_path)
-    
+
     return "\n".join(diff)
 
 
