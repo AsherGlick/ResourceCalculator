5d582730e282323abc43f301371904589569e019
Asher Glick <asher.glick@gmail.com>
2025-05-21 08:30:15 -0500

    adding comments, spacing, and help text to the resource list formatter function

--- scripts/resource_list_formatter.py
+++ scripts/resource_list_formatter.py
@@ -12,13 +12,31 @@ from pylib.resource_list import ResourceList  # noqa: E402
 from pylib.yaml_token_load import ordered_load  # noqa: E402
 
 
+################################################################################
+# main
+#
+# The main function that handles argument parsing and calling the yaml format
+# functions.
+################################################################################
 def main() -> None:
     parser = argparse.ArgumentParser(
         description='Format resources.yaml files'
     )
-    parser.add_argument("--all", action="store_true", help="Formats all of the yaml files in the resource_lists directory")
-    parser.add_argument("path", nargs='?', help="Formats a single file")
-    parser.add_argument("--inline", action="store_true", help="Formats a file and writes it to the file")
+    parser.add_argument(
+        "--all",
+        action="store_true",
+        help="Formats all of the yaml files in the resource_lists directory"
+    )
+    parser.add_argument(
+        "path",
+        nargs='?',
+        help="Formats only the file file specified"
+    )
+    parser.add_argument(
+        "--inline",
+        action="store_true",
+        help="Write the formatted data back to the input file instead of a new file"
+    )
     args = parser.parse_args()
 
     if args.all:
@@ -28,11 +46,20 @@ def main() -> None:
             format_single_file(path, same_file=True)
 
     elif args.path is not None:
-        format_single_file(args.path, args.inline)
+        format_single_file(args.path, same_file=args.inline)
+
+    else:
+        parser.print_help()
 
 
+################################################################################
+# format_single_file
+#
+# Reads a single yaml file specified by input_filepath and outputs a formatted
+# version of the file to a new file with the ".formatted" suffix. If same_file
+# is set to True then the output is written in place to the input file instead.
+################################################################################
 def format_single_file(input_filepath: str, same_file: bool = False) -> None:
-
     if same_file:
         output_filepath = input_filepath
     else:
@@ -55,13 +82,30 @@ def format_single_file(input_filepath: str, same_file: bool = False) -> None:
     print(diff_yaml(input_filepath, output_filepath))
 
 
+################################################################################
+# diff_yaml
+#
+# A sanity check function used to confirm that the formatter did not change any
+# of the actual data and just the formatting of it.
+################################################################################
 def diff_yaml(file_a_path: str, file_b_path: str) -> str:
     with open(file_a_path, 'r', encoding="utf_8") as f:
-        data_a = yaml.dump(yaml.safe_load(f.read()), default_flow_style=False).splitlines()
+        data_a = yaml.dump(
+            yaml.safe_load(f.read()),
+            default_flow_style=False
+        ).splitlines()
     with open(file_b_path, 'r', encoding="utf_8") as f:
-        data_b = yaml.dump(yaml.safe_load(f.read()), default_flow_style=False).splitlines()
+        data_b = yaml.dump(
+            yaml.safe_load(f.read()),
+            default_flow_style=False
+        ).splitlines()
 
-    diff = difflib.unified_diff(data_a, data_b, fromfile=file_a_path, tofile=file_b_path)
+    diff = difflib.unified_diff(
+        data_a,
+        data_b,
+        fromfile=file_a_path,
+        tofile=file_b_path
+    )
 
     return "\n".join(diff)
 
